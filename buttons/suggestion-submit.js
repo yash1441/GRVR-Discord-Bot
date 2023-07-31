ec0b00193b2de87bcfbc81e5467ceb36fc36e92d
Simon <yash1441@yahoo.com>
2023-07-31 21:38:12 +0530

    Updated to reply

--- buttons/suggestion-submit.js
+++ buttons/suggestion-submit.js
@@ -38,7 +38,7 @@ module.exports = {
         };
 
         message.awaitMessageComponent({ filter: collectorFilter, componentType: ComponentType.StringSelect, time: 15000 })
-            .then(interaction => interaction.editReply(`You selected ${interaction.values.join(', ')}!`))
+            .then(interaction => interaction.reply(`You selected ${interaction.values.join(', ')}!`))
             .catch(err => {
                 console.log(err);
                 logger.debug('No interactions were collected.')
