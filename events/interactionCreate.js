f74169c187e723d8c0ea2ca24df2514fcb04a63b
Simon <yash1441@yahoo.com>
2023-07-31 20:51:08 +0530

    Updated interaction handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,46 +4,60 @@ const logger = require("../logging/logger.js");
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
 
-        const command = interaction.client.commands.get(interaction.commandName);
-
-        if (!command) {
-            logger.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-
-        const { cooldowns } = interaction.client;
-
-        if (!cooldowns.has(command.data.name)) {
-            cooldowns.set(command.data.name, new Collection());
-        }
-
-        const now = Date.now();
-        const timestamps = cooldowns.get(command.data.name);
-        const defaultCooldownDuration = 3;
-        const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-        if (timestamps.has(interaction.user.id)) {
-            const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-            if (now < expirationTime) {
-                const expiredTimestamp = Math.round(expirationTime / 1000);
-                return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+            if (!command) {
+                logger.error(`No command matching ${interaction.commandName} was found.`);
+                return;
             }
-        }
 
-        timestamps.set(interaction.user.id, now);
-        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+            const { cooldowns } = interaction.client;
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            logger.error(JSON.stringify(error));
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            if (!cooldowns.has(command.data.name)) {
+                cooldowns.set(command.data.name, new Collection());
+            }
+
+            const now = Date.now();
+            const timestamps = cooldowns.get(command.data.name);
+            const defaultCooldownDuration = 3;
+            const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+            if (timestamps.has(interaction.user.id)) {
+                const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+                if (now < expirationTime) {
+                    const expiredTimestamp = Math.round(expirationTime / 1000);
+                    return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+                }
+            }
+
+            timestamps.set(interaction.user.id, now);
+            setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                logger.error(JSON.stringify(error));
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
+            }
+        } else if (interaction.isButton()) {
+            const button = interaction.client.buttons.get(interaction.customId);
+
+            if (!button) {
+                logger.error(`No button matching ${interaction.customId} was found.`);
+                return;
+            }
+
+            try {
+                await button.execute(interaction);
+            } catch (error) {
+                logger.error(error);
+                await interaction.reply({ content: 'There was an error while executing the button script !', ephemeral: true});
             }
         }
     },
