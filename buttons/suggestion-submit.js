bdcf4144fc0d0fef0c6733ea3977b6eac0785bf5
Simon <yash1441@yahoo.com>
2023-07-31 22:03:19 +0530

    Updated reply

--- buttons/suggestion-submit.js
+++ buttons/suggestion-submit.js
@@ -36,7 +36,7 @@ module.exports = {
 
         collector.on('collect', async i => {
             const selection = i.values[0];
-            await interaction.editReply(`${i.user} has selected ${selection}!`);
+            await interaction.editReply({ content: `${i.user} has selected ${selection}!`, row: [] });
         });
     },
 };
