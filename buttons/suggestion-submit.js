ef6a92cc8c02fd0e4e836972db4c37697b5fde2b
Simon <yash1441@yahoo.com>
2023-08-01 01:11:30 +0530

    Added modal

--- buttons/suggestion-submit.js
+++ buttons/suggestion-submit.js
@@ -1,4 +1,4 @@
-const { StringSelectMenuBuilder, ActionRowBuilder, ComponentType } = require('discord.js');
+const { StringSelectMenuBuilder, ActionRowBuilder, ComponentType, ModalBuilder, TextInputBuilder, TextIntputStyle, bold } = require('discord.js');
 const logger = require("../logging/logger.js");
 
 module.exports = {
@@ -6,28 +6,29 @@ module.exports = {
         name: 'suggestion-submit'
     },
     async execute(interaction) {
+        const data = {};
         const selectMenu = new StringSelectMenuBuilder()
-            .setCustomId("suggestion-category")
-            .setPlaceholder("Suggestion Category")
+            .setCustomId('suggestion-category')
+            .setPlaceholder('Suggestion Category')
             .addOptions(
                 {
-                    label: "1",
-                    value: "1",
+                    label: '1',
+                    value: '1',
                 },
                 {
-                    label: "2",
-                    value: "2",
+                    label: '2',
+                    value: '2',
                 },
                 {
-                    label: "3",
-                    value: "3",
+                    label: '3',
+                    value: '3',
                 },
             );
 
         const row = new ActionRowBuilder().addComponents(selectMenu);
 
         const response = await interaction.reply({
-            content: `**Select Suggestion Category**`,
+            content: bold('Select Suggestion Category'),
             components: [row],
             ephemeral: true
         });
@@ -35,8 +36,31 @@ module.exports = {
         const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 15_000 });
 
         collector.on('collect', async i => {
-            const selection = i.values[0];
-            await interaction.editReply({ content: `${i.user} has selected ${selection}!`, components: [] });
+            data.category = i.values[0];
+
+            const modal = new ModalBuilder()
+                .setCustomId('suggestion-modal')
+                .setTitle(data.category)
+
+            const suggestion = new TextInputBuilder()
+                .setCustomId('suggestion-modal-suggestion')
+                .setLabel('Suggestion')
+                .setPlaceholder('Explain your suggestion here in detail')
+                .setStyle(TextIntputStyle.Paragraph);
+
+            const modalRow = new ActionRowBuilder().addComponents(suggestion);
+
+            modal.addComponents(modalRow);
+            await response.edit({ content: `${i.user} has selected ${selection}!`, components: [] });
+
+            await i.showModal(modal);
         });
+
+
+        // Show Modal
+
+        // Collect Modal
+
+        // Send to Thread
     },
 };
