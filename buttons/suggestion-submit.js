49a85ef23486dfe11bf48574675357493e6c74f1
Simon <yash1441@yahoo.com>
2023-07-31 21:21:40 +0530

    Added type back

--- buttons/suggestion-submit.js
+++ buttons/suggestion-submit.js
@@ -3,7 +3,8 @@ const logger = require("../logging/logger.js");
 
 module.exports = {
     data: {
-        name: 'suggestion-submit'
+        name: 'suggestion-submit',
+        type: 'BUTTON'
     },
     async execute(interaction) {
         const selectMenu = new StringSelectMenuBuilder()
