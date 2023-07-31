3f8ba72eb0d77a3720fe73c753843858c01d658c
Simon <yash1441@yahoo.com>
2023-07-31 19:47:56 +0530

    Added command handling

--- index.js
+++ index.js
@@ -55,11 +55,15 @@ const commandFiles = fs
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    client.commands.set(command.data.name, command);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        logger.error(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
 }
 
 client.once(Events.ClientReady, c => {
-	logger.info(`Discord bot went online. Username: ${client.user.tag}`);
+    logger.info(`Discord bot went online. Username: ${client.user.tag}`);
     client.user.setPresence({
         activities: [
             {
@@ -69,6 +73,28 @@ client.once(Events.ClientReady, c => {
         ],
         status: `dnd`,
     });
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		logger.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
