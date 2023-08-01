ccbf5a12f68acfe19dd56584dceac395ec60684b
Simon <yash1441@yahoo.com>
2023-08-01 16:52:08 +0530

    Reverted message logic

--- events/threadCreate.js
+++ events/threadCreate.js
@@ -6,8 +6,7 @@ module.exports = {
     async execute(thread) {
         if (thread.parent.type != ChannelType.GuildForum || thread.parentId != process.env.VOTE_SUGGESTIONS_CHANNEL) return;
 
-        const message = await thread.messages.fetch(thread.lastMessageId);
-
-        logger.debug(message.content);
+        const messages = await thread.messages.fetch();
+        const message = messages.first()
     }
 };
