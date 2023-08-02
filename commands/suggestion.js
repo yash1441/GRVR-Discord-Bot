569e57715960f9c0bd9c112acafb450484528056
Simon <yash1441@yahoo.com>
2023-08-02 10:52:25 +0530

    Added admin perm

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -5,7 +5,8 @@ module.exports = {
 	cooldown: 60,
 	data: new SlashCommandBuilder()
 		.setName('suggestion')
-		.setDescription('Send a suggestion'),
+		.setDescription('Send a suggestion')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
 		interaction.reply({ content: 'It works.' });
 	},
